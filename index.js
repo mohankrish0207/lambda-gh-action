995f56df4b83825a65bc429d3ebaa0c97bec52e9
mohankrish0207 <86762895+mohankrish0207@users.noreply.github.com>
2022-02-23 17:42:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Welcome to Hitech!"),
+    body: JSON.stringify("Welcome to Hitech learning acadamy!"),
   }
   return response
 }
